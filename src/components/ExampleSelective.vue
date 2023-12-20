151138c65859c50b22912366303692b50162e627
mteresacastro <m.teresacastrogonzalez@gmail.com>
2023-12-20 10:02:52 +0100

    vue directives

---
+++ src/components/ExampleSelective.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1 v-if="age<18">Eres menor de edad</h1> 
+    <h1 v-else>Eres mayor de edad</h1>
+    <!-- <h1 v-show="age<18">Eres menor de edad</h1> 
+        <h1 v-show="age>18">Eres mayor de edad</h1> -->
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name:'ExampleSelective',
+    setup(){
+        let edad = 10;
+        return edad;
+    }
+})
+    
+</script>
+
+<style scoped></style>
